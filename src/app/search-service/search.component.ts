ac7657e2a81caef62246ec952b5fff48f5330a1f
mahsum97 <mahsum.yildirim97@gmail.com>
2020-05-20 14:56:21 +0200

    SearchService

--- src/app/search-service/search.component.ts
+++ src/app/search-service/search.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchService} from './search.service';
+import {User} from '../timeline/user';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
   selector: 'app-search-service',
   templateUrl: './search.component.html',
-  providers: [SearchServiceComponent],
+  providers: [SearchService],
   styleUrls: ['./search.component.css']
 })
 export class SearchServiceComponent {
+  user: User;
+  angForm: FormGroup;
 
 
+  constructor(private searchservice: SearchService, private  fb: FormBuilder) {
+    this.createSearchForm();
+  }
+
+  createSearchForm() {
+    this.angForm = this.fb.group({
+      searchText: ['', Validators.required]
+    });
+  }
+
+  getUser(): void {
+    const text = this.angForm.get('searchText').value;
+    this.searchservice.getElasticResult(text).subscribe(user => (this.user = user));
+  }
 }
--- src/app/search-service/search.service.ts
+++ src/app/search-service/search.service.ts
@@ -1,6 +1,34 @@
 import { Injectable } from '@angular/core';
+import {HandleError, HttpErrorHandler} from '../http-error-handler.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {User} from '../timeline/user';
+import {Observable} from 'rxjs';
 
 
-export class SearchServiceComponent {
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
 
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SearchService {
+
+  getElastic = 'http://34.65.38.205:9200/users/_doc/_search?q=username:';
+
+  private handleError: HandleError;
+
+  constructor(
+    private http: HttpClient,
+    httpErrorHandler: HttpErrorHandler) {
+    this.handleError = httpErrorHandler.createHandleError('TimelineService');
+  }
+  /** GET elasticsearch result */
+  getElasticResult( text: string ): Observable<User> {
+    return this.http.get<User>(this.getElastic + text);
+  }
 }
+
